7ce9f8b3584a84af961022c138d00f3d79379cd8
wachka06 <wachka06@gmail.com>
2020-11-04 08:05:58 -0800

    updated articlecontainer

--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -4,7 +4,8 @@ import Article from "./Article";
 const ArticleContainer = ({ articles }) => {
   return (
     <div className="ArticleContainer">
-      {articles.length > 0 &&
+      {articles &&
+        articles.length > 0 &&
         articles.map((article, i) => <Article key={i} article={article} />)}
     </div>
   );
