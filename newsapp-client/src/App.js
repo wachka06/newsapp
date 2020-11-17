abe688584c909c83e5f2c9a18a071291e638aa38
wachka06 <wachka06@gmail.com>
2020-11-16 18:25:46 -0800

    updated cors

--- newsapp-client/src/App.js
+++ newsapp-client/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   });
   const [readLaters, setReadLaters] = useState([]);
   const [selectedArticle, setSelectedArticle] = useState({});
-  const url = "http://newsmore.herokuapp.com";
+  const url = "http://localhost:3000";
 
   const handleChange = (e) => {
     const { name, value } = e.target;
--- newsapp-server/server.js
+++ newsapp-server/server.js
@@ -9,22 +9,7 @@ const API_KEY = process.env.NEWSAPI_API_KEY;
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: ["https://newsmore.herokuapp.com", "http://localhost:3001"],
-//     credentials: true,
-//   })
-// );
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-//   );
-//   next();
-// });
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
